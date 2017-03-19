f5127447f1d4199a8d54510217a22f016a1a17c8
Neil Okamoto <neil.okamoto@gmail.com>
2017-03-19 14:41:08 -0700

    Roll forward to alpine 3.5 and snap 1.1.0

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -x
 
-SNAP=0.15.0-beta
-SNAP_VER=v${SNAP}
+. snap-version
 
 docker build -f Dockerfile.compile -t snap-compile .
 docker run -it --rm -v `pwd`:/mnt -e SNAP_VER=${SNAP_VER} snap-compile /mnt/compile.sh
-docker build -f Dockerfile.build -t gonewest818/intelsdi-snap:${SNAP} --build-arg SNAP_VER=${SNAP_VER} .
+docker build -f Dockerfile.build -t gonewest818/intelsdi-snap:${SNAP_VER} --build-arg SNAP_VER=${SNAP_VER} .
--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -x
 
-SNAP=0.15.0-beta
-SNAP_VER=v${SNAP}
+. snap-version
 
-docker run -it --rm -p 8181:8181 gonewest818/intelsdi-snap:${SNAP} -t 0 -l 3
+docker run -it --rm -p 8181:8181 gonewest818/intelsdi-snap:${SNAP_VER} -t 0 -l 3
